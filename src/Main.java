1f4d0f6997ab3e2c4971eaefe959a37c7b427971
Tatek <tatek.ahmed443@gmail.com>
2023-09-19 12:28:14 -0500

    sharing to the world

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         int []a ={1,2,3,5};
         int missing = getMissing(a);
 
+        //There is no missing value if the value of missing variable equals
+        // with the first value of the array
         if (missing == a[0])
             System.out.println("No Missing Number!");
         else
@@ -14,13 +16,19 @@ public class Main {
     }
 
     private static int getMissing(int[] a) {
+        //Take the first element
         int previous = a[0];
+        //placeholder for the missing number
         int missing = a[0];
 
+        //start looping from second elements and
         for (int i = 1; i < a.length; i++) {
+            //check the next elements are equals to previous +1
             if (previous +1 != a[i]) {
+                //the missing value must be the current value at index i minus 1
                 missing = a[i] -1;
             }
+            //repositioning the previous value to the current index
             previous = a[i];
         }
         return missing;
